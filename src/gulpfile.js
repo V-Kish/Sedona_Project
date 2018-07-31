9b680738e64d02868c7bda2b1c770073e3724e23
Vasya Kish <vasya.kish@gmail.com>
2018-07-31 02:47:46 -0700

    Create header

---
+++ src/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var htmlmin = require('gulp-html-minifier');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: ""
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css/"))
+        .pipe(browserSync.stream());
+});
+
+/* HTML minify*/
+
+gulp.task('minify', function() {
+    gulp.src('../*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('../app/'))
+});
+
+
+
+gulp.task('default', ['serve']);
